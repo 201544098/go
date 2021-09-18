665b954e5010d7a77f31e34c73a071576e0aaa34
201544098 <sprpsa32@naver.com>
2021-09-18 15:14:03 +0900

    v0.6

--- syntax04.go
+++ syntax04.go
@@ -1,3 +1,25 @@
+// 난수 추출된 수의 소수 판정 프로그램 v0.6
+func main() {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+
+	isPrime := true
+	number := rand.Intn(150) + 2 //0과 1제외 2~151 사이의 수
+	fmt.Println("임의로 추출된 수 : ", number)
+
+	for i := 2; i < number; i++ { // 1과 number일때 loop 돌지 않음
+		if number%i == 0 {
+			isPrime = false
+			break // 첫번째 약수 발견 시 반복문 즉시 종료
+		}
+		fmt.Print(i, " ")
+	}
+	if isPrime {
+		fmt.Println(number, "는(은) 소수입니다!")
+	} else {
+		fmt.Println(number, "는(은) 소수가 아닙니다~")
+	}
+}
 // // 난수 추출된 수의 소수 판정 프로그램 v0.4
 // func main() {
 // 	seed := time.Now().Unix()
